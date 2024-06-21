f22764def584d656b3bd0dcc335392b90084568c
ankit-rawat048 <ankitsinghrawt048@gmail.com>
2024-06-21 13:17:07 +0530

    rock,paper scissors game

---
+++ app(rock_game).js
@@ -0,0 +1,24 @@
+let userScore = 0 ;
+let compScore = 0 ;
+
+const choices =document.querySelectorAll(".choice");
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice =", userChoice); 
+    //Generate computer choice
+    const compChoice =genCompChoice();
+    console.log("comp choice =", compChoice);
+}
+ 
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
